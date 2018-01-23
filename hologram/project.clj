916da1ddd245434dcecdcb6d479522a27d8f1156
Max Burkhardt <max.burkhardt@airbnb.com>
2018-01-22 21:19:08 -0800

    Version bump, update some dependencies

--- hologram/project.clj
+++ hologram/project.clj
@@ -1,11 +1,11 @@
-(defproject hologram "0.1.4"
+(defproject hologram "0.1.5"
   :description "Consumes Aurora animation data from SQS and sends it to a panel array"
   :url "https://maxb.fm"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-http "3.6.1"]
-                 [cheshire "5.7.1"]
+                 [clj-http "3.7.0"]
+                 [cheshire "5.8.0"]
                  [com.cemerick/bandalore "0.0.6"]
                  [com.outpace/config "0.10.0"]]
   :main hologram.core
